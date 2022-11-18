ef4ef0c4172b0b4698ebc8612103e42dc068eebe
Jhon Harold Hincapie Marulanda <89672206+git-devtest@users.noreply.github.com>
2022-11-17 23:27:43 -0500

    Segunda parte curso Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Mi primera app en Angular';
-  saludo = 'Saludos desde mi primer proyecto Angular.';
+  titulo = 'Hola Mundo! desde Angular';
 }
